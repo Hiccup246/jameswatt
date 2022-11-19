90276cc8bc96882ccc15ac02f4060100796e982f
James Watt <james.watt529@outlook.com>
2022-11-19 23:21:06 +0000

    Update landing styles

--- components/LandingSection.tsx
+++ components/LandingSection.tsx
@@ -1,9 +1,9 @@
 export default function LandingSection() {
     return (
         <div className="w-full bg-brown flex flex-col justify-start pt-10 pb-52">
-            <div className="w-full flex flex-row justify-around">
-                <h1 className="text-white font-header text-4xl font-bold italic">Human</h1>
-                <h1 className="text-white font-header text-4xl font-bold italic">Developer</h1>
+            <div className="w-full flex flex-row flex-wrap justify-between">
+                <h1 className="text-black text-6xl font-extrabold">Human</h1>
+                <h1 className="text-black text-6xl font-extrabold">Developer</h1>
             </div>
 
             <div className="w-64 h-64 bg-black self-center mt-32">
