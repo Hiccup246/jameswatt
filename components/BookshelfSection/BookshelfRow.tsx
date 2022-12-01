33a543f1704baa398974f5662d75a6a6134c3a2a
James Watt <james.watt529@outlook.com>
2022-12-01 11:42:07 +0000

    Refactor book shelf section

--- components/BookshelfSection/BookshelfRow.tsx
+++ components/BookshelfSection/BookshelfRow.tsx
@@ -1,45 +1,45 @@
-import React from "react"
+import React from "react";
 import Image from "next/image";
 
 export default function BookshelfRow({ book }: { book: Book }) {
-    function getBookStatusIcon(status: string) {
-      if (status === "Read") {
-        return (
-          <div className="h-5 w-5 mx-auto">
-             <Image src="/book.png"
-                    width={20}
-                    height={20}
-                    sizes="(min-width: 640px) 20px,
-                            (min-width: 785px) 20px,
-                            20px"
-                    alt="Completed Book Icon" />
-          </div>
-        )
-      } else {
-        return (
-          <div className="h-5 w-5 mx-auto">
-            <Image src="/animated-book.gif"
-                   width={20}
-                   height={20}
-                   sizes="(min-width: 640px) 20px,
-                          (min-width: 785px) 20px,
-                          20px"
-                   alt="Currently Reading Icon" />
-            </div>
-        )
-      }
-    }
+  const completedBookIcon = (
+    <div className="h-5 w-5 mx-auto">
+      <Image src="/book.png"
+             width={20}
+             height={20}
+             sizes="(min-width: 640px) 20px,
+                    (min-width: 785px) 20px,
+                    20px"
+            alt="Completed Book Icon" />
+    </div>
+  )
 
-    return (
-        <>
-          <td className="bookshelf__name-cell flex justify-start flex-col pr-2.5 break-keep align-top">
-            <h2 className="my-0 max-w-lg font-bold">{book.name}</h2>
-            <div className="text-sm">{book.author}</div>
-          </td>
-          <td className="bookshelf__genre-cell align-top text-sm max-smaller:inline">{book.genre}</td>
-          <td className="bookshelf__status-cell align-top">
-            {getBookStatusIcon(book.status)}
-          </td>
-        </>
-      )
+  const currentlyReadingIcon = (
+    <div className="h-5 w-5 mx-auto">
+      <Image src="/animated-book.gif"
+              width={20}
+              height={20}
+              sizes="(min-width: 640px) 20px,
+                    (min-width: 785px) 20px,
+                    20px"
+              alt="Currently Reading Icon" />
+    </div>
+  )
+
+  return (
+    <>
+      <td className="flex justify-start flex-col pr-2.5 break-keep align-top">
+        <h2 className="my-0 max-w-lg font-bold">{book.name}</h2>
+        <div className="text-sm">{book.author}</div>
+      </td>
+
+      <td className="align-top text-sm max-smaller:inline">{book.genre}</td>
+      
+      <td className="align-top">
+        {
+          book.status == "Read" ? completedBookIcon : currentlyReadingIcon
+        }
+      </td>
+    </>
+  )
 }
--- components/BookshelfSection/BookshelfSection.tsx
+++ components/BookshelfSection/BookshelfSection.tsx
@@ -1,57 +1,47 @@
-import React, { useState } from "react"
-import { BOOKS } from "../../constants/Books"
+import React, { useState } from "react";
+import { BOOKS } from "../../constants/Books";
 import SectionLayout from "../SectionLayout";
 import BookshelfRow from "./BookshelfRow";
 
 export default function BookshelfSection() {
-    const previewBooks = 4
-    const renderBooks = (numberOfBooks: number) => {
-        return BOOKS.slice(0, numberOfBooks).map((book: Book, index) => {
-            return (
-                <tr key={index} className="[&>td]:pt-10 [&>td]:first:pt-0">
-                    <BookshelfRow book={book} />
-                </tr>
-            )
-        })
-    }
-
-    const [buttonText, setButtonText] = useState("Show More")
-    const [books, setBooks] = useState(renderBooks(previewBooks))
-
-    const showHideBooks = () => {
-        if (buttonText === "Show More") {
-            setBooks(renderBooks(BOOKS.length))
-            setButtonText("Show Less")
-        } else {
-            setBooks(renderBooks(previewBooks))
-            setButtonText("Show More")
-        }
-    }
-
+    const previewBooks = 4;
+    const [showAllBooks, setshowAllBooks] = useState(false);
+    
     return (
         <SectionLayout isBgColorPrimary={true}>
             <h1 className="text-center text-2xl sm:text-3xl font-bold mb-14">
                 Books I&apos;m Involved With
             </h1>
 
-            <div className="bookshelf__table-wrapper bg-white p-3.5 rounded-xl">
-                <table className="bookshelf__table w-full">
+            <div className="bg-white p-3.5 rounded-xl">
+                <table className="w-full">
                     <thead>
                         <tr>
                             <th className="text-sm pb-2.5 text-left text-black">Book</th>
-                            <th className="bookshelf__genre-header text-sm pb-2.5 text-left text-black w-24 max-smaller:hidden">Genre</th>
-                            <th className="bookshelf__status-header text-sm pb-2.5 text-center text-black">Status</th>
+                            <th className="text-sm pb-2.5 text-left text-black w-24 max-smaller:hidden">Genre</th>
+                            <th className="text-sm pb-2.5 text-center text-black">Status</th>
                         </tr>
                     </thead>
-                    <tbody>{books}</tbody>
+                    <tbody>
+                        {
+                            (showAllBooks ? BOOKS : BOOKS.slice(0, previewBooks))
+                                .map((book: Book) => {
+                                    return (
+                                        <tr key={book.name} className="[&>td]:pt-10 [&>td]:first:pt-0">
+                                            <BookshelfRow book={book} />
+                                        </tr>
+                                    )
+                                })
+                        }
+                    </tbody>
                 </table>
 
                 <button
-                    aria-label="Show More Books"
-                    className="bookshelf__show-more-button border rounded-md px-2 py-1 mx-auto w-fit block hover:opacity-50 mt-8"
-                    onClick={() => showHideBooks()}
-                    >
-                    {buttonText}
+                    aria-label="Show More/Less Books"
+                    className="border rounded-md px-2 py-1 mx-auto w-fit block hover:opacity-50 mt-8"
+                    onClick={() => showAllBooks ? setshowAllBooks(false) : setshowAllBooks(true)}
+                >
+                    {showAllBooks ? "Show Less": "Show More"}
                 </button>
             </div>
         </SectionLayout>
