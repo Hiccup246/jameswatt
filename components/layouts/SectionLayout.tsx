58e8e71bd8ea1e85c2a4782dd2b34ed581e37657
James Watt <james.watt529@outlook.com>
2022-12-04 16:31:11 +0000

    Update section layout to use section html element

--- components/layouts/SectionLayout.tsx
+++ components/layouts/SectionLayout.tsx
@@ -6,9 +6,9 @@ export default function SectionLayout(props: SectionLayoutProps) {
     <div
       className={`flex w-full flex-row justify-center pt-12 ${largeBottom} ${bgColor}`}
     >
-      <div className="mx-auto flex w-9/12 flex-col justify-start">
+      <section className="mx-auto flex w-9/12 flex-col justify-start">
         {props.children}
-      </div>
+      </section>
     </div>
   );
 }
