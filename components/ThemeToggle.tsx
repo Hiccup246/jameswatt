7b13ae6edbb8d0a93e2b687f348f967ce0b5edd6
James Watt <james.watt529@outlook.com>
2022-12-05 13:57:39 +0000

    Complete theme toggle refactor

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -2,97 +2,64 @@ import { useEffect, useState } from "react";
 import MoonIcon from "./Icons/MoonIcon";
 import SunIcon from "./Icons/SunIcon";
 
-function darkModeEnabled() {
-    if (!document) return false;
-    
-    return document.documentElement.classList.contains("dark")
+function setDOMTheme(darkMode: boolean | undefined) {
+  if (!localStorage || !document) return;
+
+  if (darkMode) {
+    localStorage.theme = "light";
+    document.documentElement.classList.remove("dark");
+  } else {
+    localStorage.theme = "dark";
+    document.documentElement.classList.add("dark");
+  }
 }
 
-function setThemeForDom(darkMode:boolean | undefined) {
-    if (!localStorage || !document) return;
+function themeLabel(darkMode: boolean | undefined) {
+  if (darkMode == undefined) return <></>;
 
-    if (darkMode) {
-        localStorage.theme = "light";
-        document.documentElement.classList.remove("dark");
-    } else {
-        localStorage.theme = "dark";
-        document.documentElement.classList.add("dark");
-    }
+  if (darkMode) {
+    return "Light Mode";
+  } else {
+    return "Dark Mode";
+  }
 }
 
-function buttonText(darkMode:boolean | undefined) {
-    if (darkMode == undefined) return <></>
+function themeIcon(darkMode: boolean | undefined) {
+  if (darkMode == undefined) return <></>;
 
-    if (darkMode) {
-        return "Light Mode";
-    } else {
-        return "Dark Mode";
-    }
-}
-
-function buttonIcon(darkMode:boolean | undefined) {
-    if (darkMode == undefined) return <></>
-
-    if (darkMode) {
-        return <SunIcon />;
-    } else {
-        return <MoonIcon />;
-    }
+  if (darkMode) {
+    return <SunIcon />;
+  } else {
+    return <MoonIcon />;
+  }
 }
 
 export default function ThemeToggle() {
-    const [darkMode, setDarkMode] = useState<boolean>();
+  const [darkMode, setDarkMode] = useState<boolean>();
 
-    function toggleTheme() {
-        if (darkModeEnabled()) {
-            setDarkMode(false);
-            setThemeForDom(darkMode)
-        } else {
-            setDarkMode(true);
-            setThemeForDom(darkMode)
-        }
+  function toggleTheme() {
+    if (darkMode) {
+      setDarkMode(false);
+      setDOMTheme(darkMode);
+    } else {
+      setDarkMode(true);
+      setDOMTheme(darkMode);
     }
-  
-    function setInitialTheme() {
-        if (darkModeEnabled()) {
-            setDarkMode(true);
-        } else {
-            setDarkMode(false);
-        }
+  }
+
+  useEffect(() => {
+    if (document.documentElement.classList.contains("dark")) {
+      setDarkMode(true);
+    } else {
+      setDarkMode(false);
     }
-  
-    // function buttonText() {
-    //     if (darkMode == undefined) return <></>
+  }, []);
 
-    //     if (darkMode) {
-    //         return "Light Mode";
-    //     } else {
-    //         return "Dark Mode";
-    //     }
-    // }
-  
-    // function buttonIcon() {
-    //     if (darkMode == undefined) return <></>
+  return (
+    <button className="hover group flex justify-center" onClick={toggleTheme}>
+      <div className="mr-2 h-4 w-4">{themeIcon(darkMode)}</div>
 
-    //     if (darkMode) {
-    //         return <SunIcon />;
-    //     } else {
-    //         return <MoonIcon />;
-    //     }
-    // }
-  
-    useEffect(() => {
-        setInitialTheme();
-    }, []);
-
-    return (
-        <button
-          className="hover group flex justify-center"
-          onClick={toggleTheme}
-        >
-          <div className="mr-2 h-4 w-4">{buttonIcon(darkMode)}</div>
-
-          <span className="group-hover:underline">{buttonText(darkMode)}</span>
-        </button>
-    );
+      <span className="group-hover:underline">{themeLabel(darkMode)}</span>
+    </button>
+  );
 }
