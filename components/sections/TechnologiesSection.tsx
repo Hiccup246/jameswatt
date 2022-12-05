602fe9c2bee50935387ab92bfcf022ca54d1e08d
James Watt <james.watt529@outlook.com>
2022-12-05 14:07:44 +0000

    Make technologies slider icon pure functional

--- components/sections/TechnologiesSection.tsx
+++ components/sections/TechnologiesSection.tsx
@@ -1,39 +1,39 @@
 import SectionLayout from "../layouts/SectionLayout";
 import { GROUP1, GROUP2, GROUP3 } from "../../constants/TechnologyIcons";
 
-export default function TechnologiesSection() {
-  function slideIcon(technologyIcon: TechnologyIcon) {
-    return (
-      <div className="slide relative h-[100px] w-[var(--slider-item-width)]">
-        <div className="hover group relative h-[86px] w-[86px] duration-150 hover:-translate-y-0.5 hover:shadow-md">
-          <a
-            href={technologyIcon.link}
-            aria-label={technologyIcon.name}
-            title={technologyIcon.name}
-            className="relative z-20 mt-1 flex h-full w-full items-center justify-center rounded-md border border-lightgrey bg-white group-hover:border-0"
-          >
-            <div className="flex h-12 w-12 justify-center">
-              <title>{technologyIcon.name}</title>
+function slideIcon(technologyIcon: TechnologyIcon) {
+  return (
+    <div className="slide relative h-[100px] w-[var(--slider-item-width)]">
+      <div className="hover group relative h-[86px] w-[86px] duration-150 hover:-translate-y-0.5 hover:shadow-md">
+        <a
+          href={technologyIcon.link}
+          aria-label={technologyIcon.name}
+          title={technologyIcon.name}
+          className="relative z-20 mt-1 flex h-full w-full items-center justify-center rounded-md border border-lightgrey bg-white group-hover:border-0"
+        >
+          <div className="flex h-12 w-12 justify-center">
+            <title>{technologyIcon.name}</title>
 
-              {technologyIcon.icon}
-            </div>
-          </a>
+            {technologyIcon.icon}
+          </div>
+        </a>
 
-          <div
-            className={`
-                            group-hover:animation-play absolute
-                            top-0 left-0
-                            z-10 h-[calc(100%+2px)]
-                            w-[calc(100%+2px)] -translate-x-px -translate-y-px rounded-lg bg-tech-icon-gradient
-                            opacity-0 transition-all duration-300 ease-linear
-                            group-hover:animate-rotate group-hover:opacity-100
-                         `}
-          ></div>
-        </div>
+        <div
+          className={`
+                          group-hover:animation-play absolute
+                          top-0 left-0
+                          z-10 h-[calc(100%+2px)]
+                          w-[calc(100%+2px)] -translate-x-px -translate-y-px rounded-lg bg-tech-icon-gradient
+                          opacity-0 transition-all duration-300 ease-linear
+                          group-hover:animate-rotate group-hover:opacity-100
+                       `}
+        ></div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
+export default function TechnologiesSection() {
   return (
     <SectionLayout>
       <h1 className="mb-16 text-center text-2xl font-bold sm:text-3xl">
