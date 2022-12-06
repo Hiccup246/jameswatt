1c7bc5f8cc6eefc24e868b175e1164dd46999900
James Watt <james.watt529@outlook.com>
2022-12-06 13:05:49 +0000

    Add e2e index spec, update readme with email link todos

--- components/sections/BookshelfSection.test.tsx
+++ components/sections/BookshelfSection.test.tsx
@@ -6,3 +6,11 @@ it("renders the BookshelfSection correctly", () => {
   const tree = renderer.create(<BookshelfSection />).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+it("renders a Show More button which when clicked displays all books", () => {
+  const tree = renderer.create(<BookshelfSection />).toJSON();
+});
+
+it("When the Show More button is clicked it renders a Show Less button which when clicked shows less books", () => {
+
+});
