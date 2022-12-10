6760749be929b7b6e9ca5162aa0a0e62b877473e
James Watt <james.watt529@outlook.com>
2022-12-10 10:55:26 +0000

    Add slider component test

---
+++ components/SliderComponent.test.tsx
@@ -0,0 +1,16 @@
+import { expect, test } from "@jest/globals";
+import SliderComponent from "./SliderComponent";
+import renderer from "react-test-renderer";
+
+test("renders the SliderComponent correctly", () => {
+  const tree = renderer
+    .create(
+      <SliderComponent>
+        <div>Div 1</div>
+        <div>Div 2</div>
+      </SliderComponent>
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
