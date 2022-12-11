403632c42cae66a13267a0d9e36bd1f1f119aa48
James Watt <james.watt529@outlook.com>
2022-12-11 18:30:29 +0000

    Refactor slider component unit tests

--- components/SliderComponent.test.tsx
+++ components/SliderComponent.test.tsx
@@ -1,15 +1,17 @@
 import SliderComponent from "./SliderComponent";
 import renderer from "react-test-renderer";
 
-test("renders the SliderComponent correctly", () => {
-  const tree = renderer
-    .create(
-      <SliderComponent>
-        <div>Div 1</div>
-        <div>Div 2</div>
-      </SliderComponent>
-    )
-    .toJSON();
+describe("SliderComponent", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(
+        <SliderComponent>
+          <div>Div 1</div>
+          <div>Div 2</div>
+        </SliderComponent>
+      )
+      .toJSON();
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
+  });
 });
