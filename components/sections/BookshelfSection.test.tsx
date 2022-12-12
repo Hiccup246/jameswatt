693a3d834b77f4284d01b90ef88178dfca6c6e81
James Watt <james.watt529@outlook.com>
2022-12-12 10:58:16 +0000

    Refactor bookshelf section unit tests

--- components/sections/BookshelfSection.test.tsx
+++ components/sections/BookshelfSection.test.tsx
@@ -3,45 +3,51 @@ import renderer from "react-test-renderer";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-const headerRows = 1;
+function headerRows(): number {
+  return screen.getAllByRole("rowgroup", { hidden: true })[0].children.length;
+}
 
-test("renders the BookshelfSection correctly", () => {
-  const tree = renderer.create(<BookshelfSection />).toJSON();
+describe("BookshelfSection", () => {
+  it("renders correctly", () => {
+    const tree = renderer.create(<BookshelfSection />).toJSON();
 
-  expect(tree).toMatchSnapshot();
-});
+    expect(tree).toMatchSnapshot();
+  });
 
-test("renders the first 4 book rows by default", () => {
-  render(<BookshelfSection />);
-
-  expect(screen.getAllByRole("row").length - headerRows).toBe(4);
-});
-
-test("renders the 'Show More' button", () => {
-  render(<BookshelfSection />);
-
-  const showMoreButton = screen.getByText("Show More");
-
-  expect(showMoreButton).toBeInTheDocument();
-});
-
-describe("When a user clicks on the 'Show More' button", () => {
-  beforeEach(async () => {
+  it("renders the first 4 book rows by default", () => {
     render(<BookshelfSection />);
-    await userEvent.click(screen.getByText("Show More"));
+
+    expect(screen.getAllByRole("row").length - headerRows()).toBe(4);
   });
 
-  test("renders more than 4 books", () => {
-    expect(screen.getAllByRole("row").length - headerRows).toBeGreaterThan(4);
+  it("renders the 'Show More' button", () => {
+    render(<BookshelfSection />);
+
+    const showMoreButton = screen.getByText("Show More");
+
+    expect(showMoreButton).toBeInTheDocument();
   });
 
-  test("renders the 'Show Less' button", () => {
-    expect(screen.getByText("Show Less")).toBeInTheDocument();
-  });
+  describe("When a user clicks on the 'Show More' button", () => {
+    beforeEach(async () => {
+      render(<BookshelfSection />);
+      await userEvent.click(screen.getByText("Show More"));
+    });
 
-  test("when the show less button is clicked only the first 4 book rows are rendered", async () => {
-    await userEvent.click(screen.getByText("Show Less"));
+    it("renders more than 4 books", () => {
+      expect(screen.getAllByRole("row").length - headerRows()).toBeGreaterThan(
+        4
+      );
+    });
 
-    expect(screen.getAllByRole("row").length - headerRows).toBe(4);
+    it("renders the 'Show Less' button", () => {
+      expect(screen.getByText("Show Less")).toBeInTheDocument();
+    });
+
+    it("when the show less button is clicked only the first 4 book rows are rendered", async () => {
+      await userEvent.click(screen.getByText("Show Less"));
+
+      expect(screen.getAllByRole("row").length - headerRows()).toBe(4);
+    });
   });
 });
