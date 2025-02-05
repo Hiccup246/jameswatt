a69866e87788c0f9dbd50024c38e3ce512d070f7
James Watt <james.watt529@outlook.com>
2025-02-05 15:42:39 +0000

    Re-format project dates

--- constants/Projects.ts
+++ constants/Projects.ts
@@ -4,7 +4,7 @@ export const PROJECTS: ShowcaseProject[] = [
     projectName: "World leader leaderboard",
     highlightProject: true,
     projectImageSrc: "/worldleaderleaderboard.webp",
-    firstPublished: new Date(2025, 2, 2), // "27/05/2024"
+    firstPublished: new Date(2025, 2, 2), // "2024/05/27"
   },
   {
     websiteUrl: "https://www.gaffbingo.com",
@@ -12,69 +12,69 @@ export const PROJECTS: ShowcaseProject[] = [
       "https://apps.apple.com/us/app/gaff-bingo/id6563151400?itscg=30200&itsct=apps_box_link&mttnsubad=6563151400",
     projectName: "Gaff Bingo",
     projectImageSrc: "/gaff-bingo.webp",
-    firstPublished: new Date(2024, 15, 10), // "27/05/2024"
+    firstPublished: new Date(2024, 10, 15),
   },
   {
     websiteUrl: "https://www.orcachase.com",
     githubRepoUrl: "https://github.com/Hiccup246/orca-chase",
     projectName: "Orca Chase",
     projectImageSrc: "/orca-chase.webp",
-    firstPublished: new Date(2024, 27, 5), // "27/05/2024"
+    firstPublished: new Date(2024, 5, 27),
   },
   {
     websiteUrl: "https://www.npm-expansions.com/",
     githubRepoUrl: "https://github.com/Hiccup246/npm-expansions",
     projectName: "Npm Expansions",
     projectImageSrc: "/npm-expansions.webp",
-    firstPublished: new Date(2023, 3, 4), // "04/04/2023"
+    firstPublished: new Date(2023, 3, 4),
   },
   {
     websiteUrl: "https://github.com/Hiccup246/development-env",
     githubRepoUrl: "https://github.com/Hiccup246/development-env",
     projectName: "MacOS Development Setup",
     projectImageSrc: "/development-setup-screenshot.webp",
-    firstPublished: new Date(2023, 1, 17), // "17/02/2022"
+    firstPublished: new Date(2023, 1, 17),
   },
   {
     websiteUrl: "https://www.custom-sound-board.com/",
     githubRepoUrl: "https://github.com/Hiccup246/custom-sound-board",
     projectName: "Custom Sound Board",
     projectImageSrc: "/custom-sound-board.webp",
-    firstPublished: new Date(2022, 10, 5), // "5/11/2022"
+    firstPublished: new Date(2022, 10, 5),
   },
   {
     websiteUrl: "https://www.freeonlinetextedit.com/",
     githubRepoUrl: "https://github.com/Hiccup246/free-online-text-edit",
     projectName: "Free Online Text Edit",
     projectImageSrc: "/freeonlinetextedit.webp",
-    firstPublished: new Date(2021, 10, 30), // "30/11/2021"
+    firstPublished: new Date(2021, 10, 30),
   },
   {
     websiteUrl: "https://jameswatt-v1.vercel.app/",
     githubRepoUrl: "https://github.com/Hiccup246/jameswatt-v1",
     projectName: "Old Portfolio Site",
     projectImageSrc: "/old-jameswatt.webp",
-    firstPublished: new Date(2021, 10, 22), // "22/11/2021"
+    firstPublished: new Date(2021, 10, 22),
   },
   {
     websiteUrl: "https://www.normmacdonaldquotes.com/",
     githubRepoUrl: "https://github.com/Hiccup246/norm-macdonald-quotes",
     projectName: "Norm MacDonald Quotes",
     projectImageSrc: "/norm-macdonald-quotes.webp",
-    firstPublished: new Date(2022, 4, 26), // "26/05/2022"
+    firstPublished: new Date(2022, 4, 26),
   },
   {
     websiteUrl: "https://www.guncontrolpolicies.com/",
     githubRepoUrl: "https://github.com/Hiccup246/gun-control-policies",
     projectName: "Gun Control Policies",
     projectImageSrc: "/gun-control-policies.webp",
-    firstPublished: new Date(2022, 5, 13), // "13/06/2022""
+    firstPublished: new Date(2022, 5, 13),
   },
   {
     websiteUrl: "https://www.npmjs.com/package/james-watt-calling-card",
     githubRepoUrl: "https://github.com/Hiccup246/james-watt-calling-card",
     projectName: "James Watt Calling Card",
     projectImageSrc: "/james-watt-calling-card.webp",
-    firstPublished: new Date(2022, 9, 7), // "7/10/2022"
+    firstPublished: new Date(2022, 9, 7),
   },
 ];
