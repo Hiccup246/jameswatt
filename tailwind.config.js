3a1d0e1a91544a82ceea2ab157d7b00dcc3b0ea0
James Watt <james.watt529@outlook.com>
2025-07-06 20:47:18 +0100

    Convert colors, breakpoints and dark mode to tailwind v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,16 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    // fontFamily: {
-    //   sans: ["var(--font-roboto)"],
-    //   serif: ["var(--font-open-sans)"],
-    // },
-    colors: {
-      brown: "#FFF1E4",
-      darkbrown: "#F2DFCE",
-      white: "#FFFFFF",
-      black: "#000000",
-      grey: "#808080",
-      lightgrey: "#CCCCCC",
-      softwhite: "#d7dce2",
-      darkgrey: "#2C3639",
-      darkteal: "#3F4E4F",
-    },
     extend: {
       backgroundImage: {
         "tech-icon-gradient":
           "linear-gradient(var(--gradient-angle),#F2DFCE,#fff,silver,#F2DFCE)",
         "dark-tech-icon-gradient":
           "linear-gradient(var(--gradient-angle),#2C3639,#fff,silver,#2C3639)",
-      },
-      screens: {
-        small: "550px",
-        smaller: "400px",
       },
       keyframes: {
         "gradient-rotate": {
